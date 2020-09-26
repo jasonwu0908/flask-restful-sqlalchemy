0ea4eb9a4a6dd85678938484ecb23c6e2099be99
jasonwu <jw840908@gmail.com>
2020-09-26 15:29:32 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+# from flask_jwt_extended import jwt_manager
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+from db import db
+
+app = Flask(__name__)
+# app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.secret_key = 'jose'
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+api = Api(app)
+db.init_app(app)
+jwt = JWT(app, authenticate, identity)
+
+api.add_resource(UserRegister, '/register')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(StoreList, '/stores')
+
+if __name__ == '__main__':
+    app.run(debug=True)  # important to mention debug=True
