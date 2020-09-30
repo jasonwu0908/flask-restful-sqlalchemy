8ec5f8509634d299f216e686dc95dfec92ea3de4
jasonwu <jw840908@gmail.com>
2020-09-30 15:55:06 +0800

    flask-jwt to flask-jwt-extend and Added JWT_Claims & jwt identity & jwt_optional

--- app.py
+++ app.py
@@ -2,20 +2,19 @@ import os
 
 from flask import Flask
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 
-# from flask_jwt_extended import jwt_manager
-from security import authenticate, identity
-from resources.user import UserRegister, User
+# from security import authenticate, identity
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
 
 app = Flask(__name__)
-# app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
-app.secret_key = 'jose'
+app.secret_key = 'jose' # app.config['JWT_SECRET_KEY']
 
 @app.before_first_request
 def create_tables():
@@ -23,10 +22,19 @@ def create_tables():
 
 api = Api(app)
 db.init_app(app)
-jwt = JWT(app, authenticate, identity)
+jwt = JWTManager(app)
+
+@jwt.user_claims_loader
+def add_claim_to_jwt(identity):
+    if identity == 1:
+        return {'is_admin': True}
+    return {'is_admin': False}
+
+
 
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(ItemList, '/items')
--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
 from models.item import ItemModel
 
 
@@ -16,7 +16,7 @@ class Item(Resource):
                         help="Every item needs a store_id!"
                         )
 
-    @jwt_required()
+    @jwt_required
     def get(self, name):
         item = ItemModel.find_by_name(name)
         if item:
@@ -37,15 +37,19 @@ class Item(Resource):
 
         return item.json(), 200
 
-    @jwt_required()
+    @jwt_required
     def delete(self, name):
+        claim = get_jwt_claims()
+        if not claim['is_admin']:
+            return {'message': 'Admin privilage required.'}, 401
+
         item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
 
         return {'message': 'Item delete'}
 
-    @jwt_required()
+    @jwt_required
     def put(self, name):
         data = Item.parser.parse_args()
         item = ItemModel.find_by_name(name)
@@ -60,6 +64,13 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    @jwt_optional
     def get(self):
-        # return {'items': list(map(lambda item: item.json(), ItemModel.query.all()))}
-        return {'items': [item.json() for item in ItemModel.find_all()]}
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
+        return {
+            'items': [item['name'] for item in items], 
+            'message': 'More data available if you login'
+        }, 200
