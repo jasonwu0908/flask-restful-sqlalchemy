595ffcc8540487921fd9e032bbb95368582ad34f
jasonwu <jw840908@gmail.com>
2020-10-05 17:12:42 +0800

    Edit item put resource to add store_id

--- resources/item.py
+++ resources/item.py
@@ -65,6 +65,7 @@ class Item(Resource):
             item = ItemModel(name, **data)
         else:
             item.price = data['price']
+            item.store_id = data['store_id']
         item.save_to_db()
 
         return item.json()
