69e14e5a81d27900a73a9ae9a9b838a81bbe9356
jasonwu <jw840908@gmail.com>
2020-10-05 16:04:26 +0800

    add refresh token require and config jwt error response

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
-# from security import authenticate, identity
-from resources.user import UserRegister, User, UserLogin
+from resources.user import (
+    UserRegister, 
+    User, 
+    UserLogin, 
+    TokenRefresh
+)
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
@@ -16,13 +20,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.secret_key = 'jose' # app.config['JWT_SECRET_KEY']
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
 api = Api(app)
 db.init_app(app)
 jwt = JWTManager(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @jwt.user_claims_loader
 def add_claim_to_jwt(identity):
@@ -30,7 +34,40 @@ def add_claim_to_jwt(identity):
         return {'is_admin': True}
     return {'is_admin': False}
 
+@jwt.expired_token_loader
+def expire_token_callback():
+    return jsonify({
+        'description': 'The token has expired.', 
+        'error': 'token_expire'
+    }), 401
 
+@jwt.invalid_token_loader
+def invalid_token_callback():
+    return jsonify({
+        'description': 'Signature verification failed.', 
+        'error': 'invalid_token'
+    }), 401
+
+@jwt.unauthorized_loader
+def missing_token_callback():
+    return jsonify({
+        'description': 'Request does not contain an access token.', 
+        'error': 'authorization_required'
+    }), 401
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback():
+    return jsonify({
+        'description': 'The token is not fresh.', 
+        'error': 'fresh_token_required'
+    }), 401
+
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return jsonify({
+        'description': 'The token has been revoked.', 
+        'error': 'token_revoked'
+    }), 401
 
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
@@ -39,6 +76,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
+api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == '__main__':
     app.run(debug=True)  # important to mention debug=True
